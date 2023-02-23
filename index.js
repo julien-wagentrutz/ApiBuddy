aaaee0a5bcb04b56eccd7953f283c7010ff3910c
julien wagentrutz <julienwagentrutz@MacBook-Pro-de-julien.local>
2023-02-23 22:06:23 +0100

    add route to update KMI

--- index.js
+++ index.js
@@ -32,6 +32,67 @@ app.post('/submitKMI', jsonParser, async (req, res) => {
 				}
 			}
 		})
+	res.json({id: response.id});
+	} catch (error) {
+		console.log(error);
+	}
+})
+
+
+app.patch('/submitKMI', jsonParser, async (req, res) => {
+	const notion = new Client({ auth: "secret_EYPtGjpPPA7Nv1KnyYtrZv0IDLjsaYh7WllkocnrtYB"})
+	const databaseId = "8d5e93850aa9413ba64d518fca5deeae"
+	
+	const pageId = req.body.pageId
+	const email = req.body.email
+	const age = req.body.age
+	const metier = req.body.metier
+	const quand = req.body.quand
+	
+
+	try {
+		const response = await notion.pages.update({
+			page_id: pageId,
+			properties: {
+				title: {
+					title: [
+						{
+							text: {
+								content: email
+							}
+						}
+					]
+				},
+				age: {
+					rich_text: [
+						{
+							text: {
+								content: age
+							}
+						}
+					]
+				},
+				metier: {
+					rich_text: [
+						{
+							text: {
+								content: metier
+							}
+						}
+					]
+				},
+				temp_metier: {
+					rich_text: [
+						{
+							text: {
+								content: quand
+							}
+						}
+					]
+				}
+
+			}
+		})
 		console.log("SUCCES");
 	} catch (error) {
 		console.log(error);
