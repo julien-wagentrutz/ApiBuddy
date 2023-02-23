f91876d135d0496de9c4c2f7e61babc4c8a41fbc
julien wagentrutz <julienwagentrutz@MacBook-Pro-de-julien.local>
2023-02-23 20:03:21 +0100

    finish route for KMI

--- index.js
+++ index.js
@@ -2,8 +2,40 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const { Client } = require("@notionhq/client")
+const cors = require('cors')
+let bodyParser = require('body-parser')
+let jsonParser = bodyParser.json()
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+	res.send("geggge")
+})
+
+app.post('/submitKMI', jsonParser, async (req, res) => {
+	const notion = new Client({ auth: "secret_EYPtGjpPPA7Nv1KnyYtrZv0IDLjsaYh7WllkocnrtYB"})
+	const databaseId = "8d5e93850aa9413ba64d518fca5deeae"
+	
+	const email = req.body.email
+
+	try {
+		const response = await notion.pages.create({
+			parent: {database_id: databaseId},
+			properties: {
+				title: {
+					title: [
+						{
+							text: {
+								content: email
+							}
+						}
+					]
+				}
+			}
+		})
+		console.log("SUCCES");
+	} catch (error) {
+		console.log(error);
+	}
 })
 
 app.listen(port, () => {
